f33ac5d5b186b71a5131cd54e1aec2baf87fe7fa
ahmed <ahmedbokhari7@gmail.com>
2017-06-25 13:02:05 +0000

    first try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.action_mailer.default_url_options = { host: 'http://ahmedsarticles.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://ahmedsarticles.herokuapp.com' }
   
 end
